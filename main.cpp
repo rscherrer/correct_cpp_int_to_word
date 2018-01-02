330fb05cad5e9ac61661fbce74ae69c96e89730a
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-02 14:02:34 +0100

    Reduce cyclomatic complexity

--- main.cpp
+++ main.cpp
@@ -32,26 +32,25 @@ int do_main(const std::vector<std::string>& args)
   return 0;
 }
 
-void test()
-{
-  assert(do_main( { "main" } ) == 1);
-  assert(do_main( { "main", "too", "many" } ) == 1);
-  assert(do_main( { "main", "0" } ) == 1);
-  assert(do_main( { "main", "7" } ) == 1);
-  assert(do_main( { "main", "1" } ) == 0);
-
-  assert(int_to_word(1) == "one");
-  assert(int_to_word(2) == "two");
-  assert(int_to_word(3) == "three");
-  assert(int_to_word(4) == "four");
-  assert(int_to_word(5) == "five");
-  assert(int_to_word(6) == "six");
-}
-
 
 int main(int argc, char* argv[]) 
 {
-  test();
+  {
+    assert(do_main( { "main" } ) == 1);
+    assert(do_main( { "main", "too", "many" } ) == 1);
+    assert(do_main( { "main", "0" } ) == 1);
+    assert(do_main( { "main", "7" } ) == 1);
+    assert(do_main( { "main", "1" } ) == 0);
+  }
+  }
+    assert(int_to_word(1) == "one");
+    assert(int_to_word(2) == "two");
+    assert(int_to_word(3) == "three");
+    assert(int_to_word(4) == "four");
+    assert(int_to_word(5) == "five");
+    assert(int_to_word(6) == "six");
+  }
+
   const std::vector<std::string> args(argv, argv + argc);
   return do_main(args);
 }
