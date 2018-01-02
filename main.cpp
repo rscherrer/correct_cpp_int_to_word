7f9bddab0a04d35af171c26c2d4df6a94f021bcf
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-02 14:53:11 +0100

    Reject incomplete code coverage

--- main.cpp
+++ main.cpp
@@ -1,65 +1,21 @@
-#include <cassert>
-#include <iostream>
+//std::invalid_argument caught by value
+
 #include <string>
-#include <vector>
-
-std::string int_to_word(const int i)
-{
-  switch (i)
-  {
-    case 1: return "one";
-    case 2: return "two";
-    case 3: return "three";
-    case 4: return "four";
-    case 5: return "five";
-    case 6: return "six";
-    default: throw std::invalid_argument("i must be in range [1, 6]");
-  }
-}
-
-int do_main(const std::vector<std::string>& args)
-{
-  if (args.size() != 2) return 1;
-  try
-  {
-    const int i{std::stoi(args[1])};
-    std::cout << int_to_word(i) << '\n';
-  }
-  catch (const std::exception&)
-  {
-    return 1;
-  }
-  return 0;
-}
-
-void test_do_main()
-{
-  assert(do_main( { "main" } ) == 1);
-  assert(do_main( { "main", "too", "many" } ) == 1);
-  assert(do_main( { "main", "0" } ) == 1);
-  assert(do_main( { "main", "7" } ) == 1);
-  assert(do_main( { "main", "1" } ) == 0);
-}
-
-void test_int_to_word()
-{
-  assert(int_to_word(1) == "one");
-  assert(int_to_word(2) == "two");
-  assert(int_to_word(3) == "three");
-  assert(int_to_word(4) == "four");
-  assert(int_to_word(5) == "five");
-  assert(int_to_word(6) == "six");
-}
-
-void test()
-{
-  test_do_main();
-  test_int_to_word();
-}
+#include <iostream>
 
 int main(int argc, char* argv[]) 
 {
-  test();
-  const std::vector<std::string> args(argv, argv + argc);
-  return do_main(args);
+  if (argc == 1) return 1;
+  try
+  {
+    std::cout << std::boolalpha << (std::stoi(argv[1]) % 2 == 1) << '\n';
+  }
+  catch (const std::invalid_argument) // caught by value
+  {
+    return 1;
+  }
+  catch (const std::out_of_range& e)
+  {
+    return 1;
+  }
 }
