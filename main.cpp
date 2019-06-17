ac9e90057edd057f4967d242e01b4795ec5cf742
rscherrer <raphael.scherrer@evobio.eu>
2019-06-17 12:41:03 +0200

    finished exercise I think

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
-#include <iostream>
+#include <cassert>
 #include <string>
 #include <vector>
+#include <iostream>
 
-int main(int argc, char* argv[])
+/// Perform the main task
+int doMain(const std::vector<std::string>& args)
 {
-    if (argc != 2)
-    {
-        return 1;
-    }
-    
+    const size_t argc = args.size();
     try
     {
-        const int value{std::stoi(argv[1])};
-        switch (value)
+        if (argc != 2)
+        {
+            throw std::runtime_error("Exactly one argument must be provided.");
+        }
+
+        const int i{std::stoi(args[1])};
+
+        switch (i)
         {
             case 1: std::cout << "one\n"; break;
             case 2: std::cout << "two\n"; break;
@@ -20,7 +24,8 @@ int main(int argc, char* argv[])
             case 4: std::cout << "four\n"; break;
             case 5: std::cout << "five\n"; break;
             case 6: std::cout << "six\n"; break;
-            default: throw std::invalid_argument("value must be in range [1, 6]");
+
+            default: throw std::invalid_argument("Input number must be in range [1, 6]");
         }
     }
     catch (const std::exception& err)
@@ -28,4 +33,27 @@ int main(int argc, char* argv[])
         std::cout << err.what() << '\n';
         return 1;
     }
+    return 0;
+}
+
+/// Program that converts a number into its word
+int main(int argc, char * argv[])
+{
+
+    const std::vector<std::string> args(argv, argv + argc);
+
+    const std::string programName(args[0]);
+
+    assert(doMain( { programName } ) == 1);
+    assert(doMain( { programName, "1" } ) == 0);
+    assert(doMain( { programName, "2" } ) == 0);
+    assert(doMain( { programName, "3" } ) == 0);
+    assert(doMain( { programName, "4" } ) == 0);
+    assert(doMain( { programName, "5" } ) == 0);
+    assert(doMain( { programName, "6" } ) == 0);
+    assert(doMain( { programName, "nonsense" } ) == 1);
+    assert(doMain( { programName, "1", "2" } ) == 1);
+
+    return doMain(args);
+
 }
